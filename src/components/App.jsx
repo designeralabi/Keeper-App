5b92917e225c136ede60ebf4ddf2d548b0535f0c
designeralabi <bdlkrmalabi@gmail.com>
2021-08-13 01:06:38 -0700

    rename the note part and it files to an array type

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
-import Note from "../components/Note.jsx";
+// import note from "../components/Note.jsx";
 
 function App(){
     return <div>
         <Header />
         <Footer / >
-        <Note />
+        {/* <Note /> */}
     </div> 
 }
 
